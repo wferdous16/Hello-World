ef8028ff007880c7e016194fe3d280872ac98df9
wferdous16 <89604960+wferdous16@users.noreply.github.com>
2021-12-02 23:06:11 -0600

    Add files via upload

---
+++ CIS 2348 Homeworks/Homework 4/Zylabs 14.11.py
@@ -0,0 +1,20 @@
+def selection_sort_descend_trace(l):
+    for i in range(len(l)-1):        
+        index = i
+        for j in range(i+1, len(l)):
+            if l[index] < l[j]:
+                index = j     
+        l[i], l[index] = l[index], l[i]
+        for i in l:
+        	print(i,end=" ")
+        print()
+
+if __name__ == "__main__":
+    line = input()
+    line = line.split()
+    
+    for i in range(len(line)):
+        line[i] = int(line[i])
+    
+    selection_sort_descend_trace(line)
+    
